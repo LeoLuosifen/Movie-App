5b13fb7557bd34e11b814ad7bdee6dbe815ffd1b
LeoLuosifen <18077921025@163.com>
2025-08-03 21:04:11 +1000

    Update files which are with trendingCard.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,10 +6,18 @@ import {useRouter} from "expo-router";
 import useFetch from "@/services/useFetch";
 import {fetchMovies} from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import {getTrendingMovies} from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {
+      data: trendingMovies,
+      loading: trendingLoading,
+      error: tredingError,
+  } = useFetch(getTrendingMovies);
 
   const {
       data: movies,
@@ -26,14 +34,14 @@ export default function Index() {
         <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-            {moviesLoading ? (
+            {moviesLoading || trendingLoading ? (
                 <ActivityIndicator
                     size="large"
                     color="#0000ff"
                     className="mt-10 self-center"
                 />
-            ) : moviesError ? (
-                <Text>Error: {moviesError?.message}</Text>
+            ) : moviesError || tredingError ? (
+                <Text>Error: {moviesError?.message || tredingError?.message}</Text>
             ) : (
                 <View className="flex-1 mt-5">
                     <SearchBar
@@ -42,7 +50,25 @@ export default function Index() {
                         placeholder="Search for a movie"
                     />
 
+                    {trendingMovies && (
+                        <View className="mt-10">
+                            <Text className="text-lg text-white font-bold">Trending Movies</Text>
+                        </View>
+                    )}
+
                     <>
+                        <FlatList
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                            ItemSeparatorComponent={() => <View className="w-4" />}
+                            className="mb-4 mt-3"
+                            data={trendingMovies}
+                            renderItem={({ item, index }) => (
+                                <TrendingCard movie={item} index={index} />
+                            )}
+                            keyExtractor={(item) => item.movie_id.toString()}
+                        />
+
                         <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
 
                         <FlatList
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -45,3 +45,17 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     // if no document is found
         // create in Appwrite database -> 1
 }
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ])
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
+}
